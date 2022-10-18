7797cd808e3e096dff629141aba7f7e93601d99a
mason park <joung4342@gmail.com>
2022-10-18 21:30:55 +0900

    add login css 1

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -3,9 +3,17 @@ import styles from './login.module.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.input_body}>
+                <label htmlFor="id">아이디</label>
+                <input id="id" type="text"/>
+            </div>
+            <div className={styles.input_body}>
+                <label htmlFor="password">비밀번호</label>
+                <input id="password" type="text"/>
+            </div>
+            <button>Login</button>
+        </section>
     );
 };
 
